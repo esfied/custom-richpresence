5bc9ee1203bcf8146e0ef32caaae5dcbc31fcbb8
Kyasuu <136987425+esfied@users.noreply.github.com>
2023-06-19 12:02:51 +0800

    Update start_rich_presence.sh

--- start_rich_presence.sh
+++ start_rich_presence.sh
@@ -3,7 +3,7 @@
 while true; do
     if pgrep -f "/Applications/nameofapp.app" > /dev/null; then  # Path of the software you'll use to execute Rich Presence
         if ! pgrep -f "richpresence.py" > /dev/null; then
-            cd /Users/cassandrasanantonio/discord-rpc
+            cd /Users/nameofuser/custom-richpresence  # Location of the folder
             python3 richpresence.py &
         fi
     else
